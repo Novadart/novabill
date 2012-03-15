d40d8bd0babb4444d08dd953bf72a9fa882e3187
Risto Gligorov <risto.gligorov@novadart.com>
2012-03-15 13:57:55 +0100

    Adding ConcurrentAccessException for service methods

--- novabill-shared/src/main/java/com/novadart/novabill/shared/client/facade/ClientService.java
+++ novabill-shared/src/main/java/com/novadart/novabill/shared/client/facade/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 import com.novadart.novabill.shared.client.dto.ClientDTO;
+import com.novadart.novabill.shared.client.exception.ConcurrentAccessException;
 import com.novadart.novabill.shared.client.exception.DataAccessException;
 import com.novadart.novabill.shared.client.exception.DataIntegrityException;
 import com.novadart.novabill.shared.client.exception.InvalidArgumentException;
@@ -13,18 +14,18 @@ import com.novadart.novabill.shared.client.exception.NotAuthenticatedException;
 @RemoteServiceRelativePath("client.rpc")
 public interface ClientService extends RemoteService{
 
-	public List<ClientDTO> getAll() throws NotAuthenticatedException;
+	public List<ClientDTO> getAll() throws NotAuthenticatedException, ConcurrentAccessException;
 	
-	public void remove(Long id) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException, DataIntegrityException;
+	public void remove(Long id) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException, DataIntegrityException, ConcurrentAccessException;
 	
-	public Long add(ClientDTO clientDTO) throws NotAuthenticatedException;
+	public Long add(ClientDTO clientDTO) throws NotAuthenticatedException, ConcurrentAccessException;
 	
-	public void update(ClientDTO clientDTO) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException;
+	public void update(ClientDTO clientDTO) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException, ConcurrentAccessException;
 	
-	public ClientDTO get(Long id) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException;
+	public ClientDTO get(Long id) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException, ConcurrentAccessException;
 	
-	public ClientDTO getFromInvoiceId(Long invoiceId) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException;
+	public ClientDTO getFromInvoiceId(Long invoiceId) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException, ConcurrentAccessException;
 	
-	public List<ClientDTO> searchClients(String query) throws InvalidArgumentException;
+	public List<ClientDTO> searchClients(String query) throws InvalidArgumentException, NotAuthenticatedException, ConcurrentAccessException;
 	
 }
--- novabill-shared/src/main/java/com/novadart/novabill/shared/client/facade/EstimationService.java
+++ novabill-shared/src/main/java/com/novadart/novabill/shared/client/facade/EstimationService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 import com.novadart.novabill.shared.client.dto.EstimationDTO;
+import com.novadart.novabill.shared.client.exception.ConcurrentAccessException;
 import com.novadart.novabill.shared.client.exception.DataAccessException;
 import com.novadart.novabill.shared.client.exception.NoSuchObjectException;
 import com.novadart.novabill.shared.client.exception.NotAuthenticatedException;
@@ -12,14 +13,14 @@ import com.novadart.novabill.shared.client.exception.NotAuthenticatedException;
 @RemoteServiceRelativePath("estimation.rpc")
 public interface EstimationService extends RemoteService {
 	
-	public EstimationDTO get(long id) throws NotAuthenticatedException, DataAccessException, NoSuchObjectException;
+	public EstimationDTO get(long id) throws NotAuthenticatedException, DataAccessException, NoSuchObjectException, ConcurrentAccessException;
 	
-	public List<EstimationDTO> getAllForClient(long clientId) throws NotAuthenticatedException, DataAccessException, NoSuchObjectException;
+	public List<EstimationDTO> getAllForClient(long clientId) throws NotAuthenticatedException, DataAccessException, NoSuchObjectException, ConcurrentAccessException;
 	
-	public Long add(EstimationDTO estimationDTO) throws NotAuthenticatedException, DataAccessException;
+	public Long add(EstimationDTO estimationDTO) throws NotAuthenticatedException, DataAccessException, ConcurrentAccessException;
 	
-	public void remove(Long id) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException;
+	public void remove(Long id) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException, ConcurrentAccessException;
 	
-	public void update(EstimationDTO estimationDTO) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException;
+	public void update(EstimationDTO estimationDTO) throws DataAccessException, NotAuthenticatedException, NoSuchObjectException, ConcurrentAccessException;
 	
 }
