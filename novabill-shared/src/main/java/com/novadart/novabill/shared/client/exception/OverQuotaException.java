0a5ddbdb4c707f3dcd464220d833fa951b89ea31
Riste Gligorov <r.gligorov@vu.nl>
2012-05-16 14:49:28 +0200

    Mechanism for enforcing quotas: 2

---
+++ novabill-shared/src/main/java/com/novadart/novabill/shared/client/exception/OverQuotaException.java
@@ -0,0 +1,9 @@
+package com.novadart.novabill.shared.client.exception;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class OverQuotaException extends Exception implements IsSerializable {
+
+	private static final long serialVersionUID = -9025048459126219816L;
+
+}
