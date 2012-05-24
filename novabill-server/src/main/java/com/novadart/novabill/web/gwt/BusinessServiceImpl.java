c0e75ffc1b9e4f3380e502d423d4b99b76a8a8b6
giordano.battilana <giordano.battilana@novadart.com>
2012-05-24 19:02:51 +0200

    purge unused import

--- novabill-server/src/main/java/com/novadart/novabill/web/gwt/BusinessServiceImpl.java
+++ novabill-server/src/main/java/com/novadart/novabill/web/gwt/BusinessServiceImpl.java
@@ -4,13 +4,12 @@ import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.novadart.novabill.domain.Business;
 import com.novadart.novabill.domain.BusinessDTOFactory;
 import com.novadart.novabill.domain.Invoice;
