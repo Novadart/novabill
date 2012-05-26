1238206b9f77190c0a592ab4e83394a09451a6e1
giordano.battilana <giordano.battilana@novadart.com>
2012-05-26 15:02:17 +0200

    make the pdf window a bit nicer

--- novabill-client/src/main/java/com/novadart/novabill/frontend/client/util/PDFUtils.java
+++ novabill-client/src/main/java/com/novadart/novabill/frontend/client/util/PDFUtils.java
@@ -7,18 +7,20 @@ public class PDFUtils {
 
 	private static final String PDF_REQUEST = 
 			GWT.getHostPageBaseURL()+"private/pdf/landing/{document}/{id}";
+	
+	private static final String POPUP_PARAMS = "width=380px,height=260px,top=10px,left=10px,status=0,menubar=0,scrollbars=0,location=0";
 
 	
 	public static void generateInvoicePdf(final long id){
 		Window.open(
 				PDF_REQUEST.replace("{document}","invoices").replace("{id}", String.valueOf(id))
-				, null, "width=300px,height=200px;");
+				, null, POPUP_PARAMS);
 	}
 	
 	public static void generateEstimationPdf(final long id){
 		Window.open(
 				PDF_REQUEST.replace("{document}","estimations").replace("{id}", String.valueOf(id))
-				, null, "width=300px,height=200px;");
+				, "_blank", POPUP_PARAMS);
 	}
 	
 }
