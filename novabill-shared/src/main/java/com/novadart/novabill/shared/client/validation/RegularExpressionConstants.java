d5f4be57859abac8aac2a13b2844147e2104e532
Risto Gligorov <risto.gligorov@novadart.com>
2012-06-08 21:10:09 +0200

    Feature #259

---
+++ novabill-shared/src/main/java/com/novadart/novabill/shared/client/validation/RegularExpressionConstants.java
@@ -0,0 +1,9 @@
+package com.novadart.novabill.shared.client.validation;
+
+public class RegularExpressionConstants {
+	
+	public static final String VAT_ID_REGEX = "^\\w{2}[0-9]{11}$";
+	
+	public static final String SSN_REGEX = "^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$";
+
+}
