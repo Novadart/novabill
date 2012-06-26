7e81bd74fd0fbda660673806cdc49849c085b9e6
Risto Gligorov <risto.gligorov@gmail.com>
2012-06-26 20:01:49 +0200

    Fixed feature #274

--- novabill-server/src/main/java/com/novadart/novabill/service/PayPalSubscriptionIPNHandlerService.java
+++ novabill-server/src/main/java/com/novadart/novabill/service/PayPalSubscriptionIPNHandlerService.java
@@ -1,18 +1,16 @@
 package com.novadart.novabill.service;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.novadart.novabill.annotation.MailMixin;
 import com.novadart.novabill.domain.Business;
 import com.novadart.novabill.domain.security.RoleTypes;
-import com.novadart.novabill.web.mvc.PayPalIPNListenerController.NameValuePair;
 
 
 @Service
@@ -20,6 +18,8 @@ import com.novadart.novabill.web.mvc.PayPalIPNListenerController.NameValuePair;
 public class PayPalSubscriptionIPNHandlerService {
 	
 	public static final String SIGNUP = "subscr_signup";
+	
+	public static final String PAYMENT = "subscr_payment";
 	
 	private final static String COMPLETED = "Completed";
 	
@@ -35,28 +35,24 @@ public class PayPalSubscriptionIPNHandlerService {
 	@Autowired
 	private PrincipalDetailsService principalDetailsService;
 	
-	private Map<String, String> createParametersMap(List<NameValuePair> parameters){
-		Map<String, String> map = new HashMap<String, String>();
-		for(NameValuePair pair: parameters)
-			map.put(pair.getName(), pair.getValue());
-		return map;
-	}
-	
-	public void handle(String transactionType, List<NameValuePair> parameters){
-		Map<String, String> parametersMap = createParametersMap(parameters);
-		if(transactionType.equals(SIGNUP))
-			handleSignup(parametersMap);
+	public void handle(String transactionType, Map<String, String> parametersMap){
+		if(transactionType.equals(PAYMENT)){
+			LOGGER.info("Handling IPN: payment");
+			handlePayment(parametersMap);
+		}
 	}
 	
 	@Transactional(readOnly = false)
-	private void handleSignup(Map<String, String> parametersMap){
+	private void handlePayment(Map<String, String> parametersMap){
 		if(!parametersMap.get(PAYMENT_STATUS).equals(COMPLETED)) return;//not completed ignore
-		String email = parametersMap.get(RECEIVER_EMAIL); 
+		String email = parametersMap.get(RECEIVER_EMAIL);
 		if(!email.equals(payPalEmail)) return; //email doesn't match
 		//TODO do additional checks here eg amount
 		Business business = principalDetailsService.loadUserByUsername(parametersMap.get(CUSTOM)).getPrincipal();
-		business.getGrantedRoles().remove(RoleTypes.ROLE_BUSINESS_FREE);
-		business.getGrantedRoles().add(RoleTypes.ROLE_BUSINESS_PREMIUM);
+		if(!business.getGrantedRoles().contains(RoleTypes.ROLE_BUSINESS_PREMIUM)){
+			business.getGrantedRoles().remove(RoleTypes.ROLE_BUSINESS_FREE);
+			business.getGrantedRoles().add(RoleTypes.ROLE_BUSINESS_PREMIUM);
+		}
 		sendMessage(parametersMap.get(CUSTOM), "Account upgraded", new HashMap<String, Object>(), "mail-templates/upgrade-notification.vm");
 	}
 
--- novabill-server/src/main/java/com/novadart/novabill/web/mvc/PayPalIPNListenerController.java
+++ novabill-server/src/main/java/com/novadart/novabill/web/mvc/PayPalIPNListenerController.java
@@ -7,14 +7,19 @@ import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
-import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -44,75 +49,49 @@ public class PayPalIPNListenerController {
 
     private final static String RESP_VERIFIED = "VERIFIED";
     
-	private boolean verifyIPN(String queryString) throws URISyntaxException, ClientProtocolException, IOException{
-    	//passing back the message to paypal
-    	URIBuilder builder = new URIBuilder(payPalUrl);
-    	builder.setQuery(String.format("%s=%s&%s", PARAM_NAME_CMD, PARAM_VAL_CMD, queryString));
-    	HttpClient httpClient = new DefaultHttpClient();
-    	HttpPost httpPost = new HttpPost(builder.build());
-    	httpPost.setHeader(CONTENT_TYPE, MIME_APP_URLENC);
-    	HttpResponse response = httpClient.execute(httpPost);
-    	if(response.getStatusLine().getStatusCode() != 200)
-    		return false;
-    	InputStream is = response.getEntity().getContent();
+	@SuppressWarnings("unchecked")
+	private boolean verifyIPN(HttpServletRequest request) throws URISyntaxException, ClientProtocolException, IOException{
+		//passing back the message to paypal
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost(payPalUrl);
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair(PARAM_NAME_CMD, PARAM_VAL_CMD)); //You need to add this parameter to tell PayPal to verify
+		for (Enumeration<String> e = request.getParameterNames(); e.hasMoreElements();) {
+			String name = e.nextElement();
+			String value = request.getParameter(name);
+			params.add(new BasicNameValuePair(name, value));
+		}
+		post.setEntity(new UrlEncodedFormEntity(params, request.getCharacterEncoding() == null? "utf-8": request.getCharacterEncoding()));
+		InputStream is = client.execute(post).getEntity().getContent();
     	BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         String responseText = reader.readLine();
         is.close();
-    	return responseText.equals(RESP_VERIFIED);
-    	
+    	return RESP_VERIFIED.equals(responseText);
     }
     
     @SuppressWarnings("unchecked")
-	private List<NameValuePair> extractParameters(HttpServletRequest request){
+	private Map<String, String> extractParameters(HttpServletRequest request){
+    	Map<String, String> parametersMap = new HashMap<String, String>();
     	Enumeration<String> paramNames = request.getParameterNames();
-    	List<NameValuePair> parameters = new ArrayList<PayPalIPNListenerController.NameValuePair>();
     	while(paramNames.hasMoreElements()){
-    		String param = paramNames.nextElement();
-    		parameters.add(new NameValuePair(param, request.getParameter(param)));
+    		String paramName = paramNames.nextElement();
+    		parametersMap.put(paramName, request.getParameter(paramName));
     	}	
-    	return parameters;
+    	return parametersMap;
     }
     
     @RequestMapping
     public @ResponseBody void processIPN(@RequestParam("txn_type") String transactionType, @RequestParam(value = "txn_id", required = false) String transactionID,
     		HttpServletRequest request) throws URISyntaxException, ClientProtocolException, IOException{
-    	List<NameValuePair> parameters = extractParameters(request);
-    	if(!verifyIPN(request.getQueryString())) return;
-    	if(transactionID != null && PayPalTransactionID.findByTransactionID(transactionID).size() > 0) return; //already processed thus ignore
-    	subscriptionHandler.handle(transactionType, parameters);
-    	if(transactionID != null)
-    		new PayPalTransactionID(transactionID).merge(); //store transaction id
-    }
-    
-    public static class NameValuePair{
-    	
-    	private String name;
-    	
-    	private String value;
-    	
-    	public NameValuePair(String name, String value){
-    		this.name = name;
-    		this.value = value;
+    	Map<String, String> parametersMap = extractParameters(request);
+    	if(!verifyIPN(request)) return;
+    	if(transactionID != null){
+    		if(PayPalTransactionID.findByTransactionID(transactionID).size() > 0)
+    			return; //already processed thus ignore
+    		else
+    			new PayPalTransactionID(transactionID).merge(); //store transaction id
     	}
-    	
-    	public NameValuePair(){}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getValue() {
-			return value;
-		}
-
-		public void setValue(String value) {
-			this.value = value;
-		}
-    	
+    	subscriptionHandler.handle(transactionType, parametersMap);
     }
 
 }
