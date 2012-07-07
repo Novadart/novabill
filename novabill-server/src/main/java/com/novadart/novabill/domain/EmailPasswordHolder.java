bdd0264aea36fb72cb66c7cbdea0cf6183d752c0
Risto Gligorov <risto.gligorov@gmail.com>
2012-07-08 00:37:09 +0200

    Feature #277

---
+++ novabill-server/src/main/java/com/novadart/novabill/domain/EmailPasswordHolder.java
@@ -0,0 +1,193 @@
+package com.novadart.novabill.domain;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Version;
+import javax.validation.constraints.Size;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.transaction.annotation.Transactional;
+import com.novadart.novabill.annotation.Hash;
+
+@Configurable
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public class EmailPasswordHolder {
+	
+	@NotEmpty(message = "{required.email}")
+	@Email(message = "{invalid.format.email}")
+	protected String email;
+	
+	@NotEmpty(message = "{required.password}")
+	protected String password;
+	
+	protected String confirmPassword;
+	
+	protected Date expirationDate;
+	
+	protected Long creationTime = System.currentTimeMillis();
+	
+	@Size(max = 64)
+	protected String activationToken;
+	
+	/*
+	 * Getters and setters
+	 * */
+	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	@Hash(saltMethod = "getCreationTime")
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	@Hash(saltMethod = "getCreationTime")
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+	
+	public void setExpirationDate(Date expirationDate){
+		this.expirationDate = expirationDate;
+	}
+	
+	public Long getCreationTime() {
+		return creationTime;
+	}
+	
+	public void setCreationTime(Long creationTime){
+		this.creationTime = creationTime;
+	}
+
+	public String getActivationToken() {
+		return activationToken;
+	}
+
+	public void setActivationToken(String activationToken) {
+		this.activationToken = activationToken;
+	}
+	/*
+	 * End of getters and setters section
+	 * */
+	
+	/*
+	 * Active record functionality
+	 * */
+	
+	@PersistenceContext
+    transient EntityManager entityManager;
+    
+    public static final EntityManager entityManager() {
+        EntityManager em = new Registration().entityManager;
+        if (em == null) throw new IllegalStateException("Entity manager has not been injected (is the Spring Aspects JAR configured as an AJC/AJDT aspects library?)");
+        return em;
+    }
+
+    public static EmailPasswordHolder findEmailPasswordHolder(Long id) {
+        if (id == null) return null;
+        return entityManager().find(EmailPasswordHolder.class, id);
+    }
+    
+    @Transactional
+    public void persist() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.persist(this);
+    }
+    
+    @Transactional
+    public void remove() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        if (this.entityManager.contains(this)) {
+            this.entityManager.remove(this);
+        } else {
+        	EmailPasswordHolder attached = EmailPasswordHolder.findEmailPasswordHolder(this.id);
+            this.entityManager.remove(attached);
+        }
+    }
+    
+    @Transactional
+    public void flush() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.flush();
+    }
+    
+    @Transactional
+    public void clear() {
+        if (this.entityManager == null) this.entityManager = entityManager();
+        this.entityManager.clear();
+    }
+    
+	/*
+	 * End of active record functionality section
+	 * */
+	
+	
+	/*
+     * Entity
+     * */
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    
+    @Version
+    @Column(name = "version")
+    private Integer version;
+    
+    public Long getId() {
+        return this.id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Integer getVersion() {
+        return this.version;
+    }
+    
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+    
+    /*
+     * End of entity section
+     * */
+    
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+}
--- novabill-server/src/main/java/com/novadart/novabill/service/validator/ForgotPasswordValidator.java
+++ novabill-server/src/main/java/com/novadart/novabill/service/validator/ForgotPasswordValidator.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 import com.novadart.novabill.domain.Business;
-import com.novadart.novabill.domain.Registration;
+import com.novadart.novabill.domain.ForgotPassword;
 
 @Service
-public class RegistrationValidator {
+public class ForgotPasswordValidator {
 	
 	@Autowired
 	private EmailPasswordHolderValidator validator;
 	
-	public void validate(Registration registration, Errors errors){
-		validator.validate(registration, errors);
-		if(Business.findByEmail(registration.getEmail()) != null)
-				errors.rejectValue("email", "email.exists");
+	public void validate(ForgotPassword forgotPassword, Errors errors){
+		validator.validate(forgotPassword, errors);
+		if(Business.findByEmail(forgotPassword.getEmail()) == null)
+				errors.rejectValue("email", "email.not.exists");
 	}
 
 }
