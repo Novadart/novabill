0a492107b6652f1ed4546ae532661da4d7679956
giordano.battilana <giordano.battilana@novadart.com>
2012-09-20 00:27:39 +0200

    refactor default template

--- novabill-report/src/main.py
+++ novabill-report/src/main.py
@@ -1,11 +1,13 @@
 # coding: utf-8
+from lib.data import InvoiceData
 from os import remove
 from os.path import exists
-from template.core import DefaultDirector, DocumentType, DirectorType,\
+from template.core import DefaultDirector, DocumentType, DirectorType, \
     BuilderType
-from template.default import DefaultInvoiceBuilder, DefaultEstimationBuilder,\
-    DefaultCreditNoteBuilder, DefaultTransportDocumentBuilder
-from lib.data import InvoiceData
+from template.default.credit_note import DefaultCreditNoteBuilder
+from template.default.estimation import DefaultEstimationBuilder
+from template.default.invoice import DefaultInvoiceBuilder
+from template.default.transport_document import DefaultTransportDocumentBuilder
 
 class Factory(object):
     
@@ -38,81 +40,9 @@ def create_doc(out, invoice, pathToLogo=None, logoWidth=None, logoHeight=None, d
     director.construct()
         
         
-if False and __name__ == '__main__':
+if __name__ == '__main__':
     testInvoiceJSON  = """
-        {
-            "class":"com.novadart.novabill.domain.Invoice",
-            "accountingDocumentDate":"10/08/2012",
-            "accountingDocumentYear":2012,
-            "business":{
-                "address":"via Stradone, 51",
-                "city":"Campo San Martino",
-                "class":"com.novadart.novabill.domain.Business",
-                "country":"Italia",
-                "creationTime":null,
-                "email":"giordano.battilana@novadart.com",
-                "fax":"0498597898","id":1,
-                "lastLogin":1344589157483,
-                "logo":null,
-                "mobile":"3334927614",
-                "name":"Novadart S.n.c. di Giordano Battilana &amp; C.",
-                "nextEstimationDocumentID":1,
-                "nextInvoiceDocumentID":2,
-                "nonFreeAccountExpirationTime":null,
-                "password":"17f3fdc0520bbf0588b41bf45c0d68ad0da26c80d3dc466a96a8215b2a4de187",
-                "phone":"3334927614",
-                "postcode":"35010",
-                "province":"PD",
-                "ssn":"IT04534730280",
-                "vatID":"IT04534730280",
-                "version":4,
-                "web":""
-            },
-            "client":{
-                "address":"via Vezzi, 12",
-                "business":{"address":"via Stradone, 51","city":"Campo San Martino","class":"com.novadart.novabill.domain.Business","country":"Italia","creationTime":null,"email":"giordano.battilana@novadart.com","fax":"0498597898","id":1,"lastLogin":1344589157483,"logo":null,"mobile":"3334927614","name":"Novadart S.n.c. di Giordano Battilana &amp; C.","nextEstimationDocumentID":1,"nextInvoiceDocumentID":2,"nonFreeAccountExpirationTime":null,"password":"17f3fdc0520bbf0588b41bf45c0d68ad0da26c80d3dc466a96a8215b2a4de187","phone":"3334927614","postcode":"35010","province":"PD","ssn":"IT04534730280","vatID":"IT04534730280","version":4,"web":""},
-                "city":"Nervesa della Battaglia",
-                "class":"com.novadart.novabill.domain.Client",
-                "country":"Italia",
-                "email":"",
-                "fax":"",
-                "id":2,
-                "mobile":"",
-                "name":"General Alarm Eredi sas di Borsato Andrea",
-                "phone":"",
-                "postcode":"31040",
-                "province":"TV",
-                "ssn":"",
-                "vatID":"04085780262",
-                "version":2,
-                "web":""
-            },
-            "documentID":1,
-            "id":1,
-            "accountingDocumentItems":[
-                {
-                    "class":"com.novadart.novabill.domain.AccountingDocumentItem",
-                    "description":"1",
-                    "id":7,
-                    "price":"1.00",
-                    "quantity":"1.00",
-                    "tax":"21.00",
-                    "total":"1.21",
-                    "totalBeforeTax":"1.00",
-                    "totalTax":"0.21",
-                    "unitOfMeasure":"","version":0
-                }
-            ],
-            "note":"",
-            "payed":false,
-            "paymentDueDate":"10/08/2012",
-            "paymentNote":"",
-            "paymentType":"CASH",
-            "total":"1.21",
-            "totalBeforeTax":"1.00",
-            "totalTax":"0.21",
-            "version":0
-        }
+        {"accountingDocumentDate":"19/09/2012","accountingDocumentItems":[{"class":"com.novadart.novabill.domain.AccountingDocumentItem","description":"1","id":8,"price":"1.00","quantity":"1.00","tax":"21.00","total":"1.21","totalBeforeTax":"1.00","totalTax":"0.21","unitOfMeasure":"1","version":0}],"accountingDocumentYear":2012,"business":{"address":"via Stradone, 51","city":"Campo San Martino","class":"com.novadart.novabill.domain.Business","country":"Italia","creationTime":null,"email":"giordano.battilana@novadart.com","fax":"0498597898","id":1,"lastLogin":1348089409855,"logo":null,"mobile":"3334927614","name":"Novadart S.n.c. di Giordano Battilana &amp; C.","nextCreditNoteDocumentID":1,"nextEstimationDocumentID":1,"nextInvoiceDocumentID":2,"nextTransportDocDocumentID":1,"nonFreeAccountExpirationTime":null,"password":"17f3fdc0520bbf0588b41bf45c0d68ad0da26c80d3dc466a96a8215b2a4de187","phone":"3334927614","postcode":"35010","province":"PD","ssn":"IT04534730280","vatID":"IT04534730280","version":3,"web":""},"class":"com.novadart.novabill.domain.Invoice","client":{"address":"via Vezzi, 12","business":{"address":"via Stradone, 51","city":"Campo San Martino","class":"com.novadart.novabill.domain.Business","country":"Italia","creationTime":null,"email":"giordano.battilana@novadart.com","fax":"0498597898","id":1,"lastLogin":1348089409855,"logo":null,"mobile":"3334927614","name":"Novadart S.n.c. di Giordano Battilana &amp; C.","nextCreditNoteDocumentID":1,"nextEstimationDocumentID":1,"nextInvoiceDocumentID":2,"nextTransportDocDocumentID":1,"nonFreeAccountExpirationTime":null,"password":"17f3fdc0520bbf0588b41bf45c0d68ad0da26c80d3dc466a96a8215b2a4de187","phone":"3334927614","postcode":"35010","province":"PD","ssn":"IT04534730280","vatID":"IT04534730280","version":3,"web":""},"city":"Nervesa della Battaglia","class":"com.novadart.novabill.domain.Client","country":"Italia","email":"","fax":"","id":2,"mobile":"","name":"General Alarm Eredi sas di Borsato Andrea","phone":"","postcode":"31040","province":"TV","ssn":"","vatID":"04085780262","version":2,"web":""},"documentID":1,"id":7,"note":"1","payed":false,"paymentDueDate":null,"paymentNote":null,"paymentType":null,"total":"1.21","totalBeforeTax":"1.00","totalTax":"0.21","version":0}
     """
     outputFile = "/tmp/testInvoice.pdf"
     if exists(outputFile):
