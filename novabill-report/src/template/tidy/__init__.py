77c1f5c31d7dd9b42abe14a9838e22ab464169a6
Risto Gligorov <risto.gligorov@novadart.com>
2012-10-03 13:14:05 +0200

    Pdf generation

--- novabill-report/src/template/tidy/__init__.py
+++ novabill-report/src/template/tidy/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from reportlab.lib.colors import gray
 from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.units import inch
+from reportlab.lib.units import cm
 from reportlab.platypus.doctemplate import BaseDocTemplate, SimpleDocTemplate
 from reportlab.platypus.flowables import Spacer, Flowable, Image
 from reportlab.platypus.paragraph import Paragraph
@@ -15,7 +15,9 @@ from reportlab.lib.pagesizes import A4
 
 BORDER_SIZE = 0.5
 BORDER_COLOR = gray
-
+LARGE_FONT_SIZE = 14
+MEDIUM_FONT_SIZE = 12
+SMALL_FONT_SIZE = 10 
 
 class TidyDirector(AbstractDirector):
     
@@ -24,20 +26,29 @@ class TidyDirector(AbstractDirector):
         doc, data = builder.getDocument(), self.getData()
         story = []
         if data.getBusiness().getLogo():
-            story.append(builder.getLogoFlowable(inch, inch)) #place logo at the top
-        else:
-            story.append(builder.getNoLogoFlowable(inch, inch))
+            story.append(builder.getLogoFlowable(2.5*cm, 2.5*cm)) #place logo at the top
+#        else:
+#            story.append(builder.getNoLogoFlowable(2.5*cm, 2.5*cm))
         
-        story.append(builder.getDocumentDetailsFlowable(data, doc.width *0.3, 0.5))
-        
-#        story.append(Table([[builder.getBusinessFlowable(data.getBusiness(), doc.width / 2),
-#                             builder.getCustomerFlowable(data.getClient(), doc.width / 2)]],
-#                           colWidths=[doc.width / 2] * 2))
-#        story.append(builder.getVerticalSpacerFlowable(5))
-#        story.append(builder.getDocumentDetailsFlowable(data, doc.width, 0.3))
-#        story.append(builder.getVerticalSpacerFlowable(10))
-#        story.append(builder.getDocumentItemsFlowable(data.getAccountingDocumentItems(), doc.width))
-#        story.append(builder.getFooterFlowable(data, doc.width))
+        header = Table([[builder.getBusinessNameFlowable(data.getBusiness()),
+                         builder.getDocumentDetailsFlowable(data, doc.width*0.4)]], colWidths=[doc.width*0.6, doc.width*0.4])
+        header.setStyle(TableStyle([("VALIGN", (0, 0), (-1, -1), "TOP")]))
+        story.append(header)
+        story.append(Spacer(1, 2*cm))
+        toFrom = Table([[builder.getToEndpointFlowable(data), builder.getFromEndpointFlowable(data)]],
+                       colWidths=[doc.width*0.5, doc.width*0.5])
+        toFrom.setStyle(TableStyle([("VALIGN", (0, 0), (-1, -1), "TOP"),
+                                    ("ALIGN", (0, 0), (-1, -1), "LEFT"),
+                                    ("LEFTPADDING", (0, 0), (-1, -1), 1.75*cm)]))
+        story.append(toFrom)
+        story.append(Spacer(1, 2*cm))
+        story.append(builder.getDocumentItemsFlowable(data.getAccountingDocumentItems(), doc.width))
+        story.append(Spacer(1, cm))
+        totals = Table([["", builder.getDocumentTotals(data, doc.width*0.4)], ["",""]], colWidths=[doc.width*0.6, doc.width*0.4])
+        totals.setStyle(TableStyle([("LINEBELOW", (0,-1), (-1,-1), BORDER_SIZE, BORDER_COLOR)]))
+        story.append(totals)
+        story.append(Spacer(1, cm))
+        story.append(builder.getNotesFlowable(data))
         doc.build(story, canvasmaker=instatiateCanvasMaker(pagenumbers=self.getDispParams()["pagenumbers"] \
                                                            if "pagenumbers" in self.getDispParams() else None,
                                                            watermark=self.getDispParams()["watermark"] \
@@ -47,7 +58,7 @@ class TidyDirector(AbstractDirector):
 class TidyDocumentBuilder(object):
     
     def __init__(self, outputFilePath, dispParams=dict()):
-        self.__doc = SimpleDocTemplate(outputFilePath, pagesize=A4)
+        self.__doc = SimpleDocTemplate(outputFilePath, pagesize=A4, rightMargin=18,leftMargin=18, topMargin=18,bottomMargin=100)
         self.__displayParams = dispParams
         
     def getDocument(self):
@@ -62,32 +73,50 @@ class TidyDocumentBuilder(object):
         return im
     
     def getNoLogoFlowable(self, height, width):
-        return Spacer(1, inch);
+        return Spacer(1, 3*cm);
     
-    def getDocumentDetailsFlowable(self, data, width, ratio):
-        #TODO this has to be moved to sub implementations
+    def getBusinessNameFlowable(self, business):
         style = getSampleStyleSheet()["Normal"]
-        tableFlowables = [
-            [Paragraph("", style), Paragraph("<b>FATTURA</b> "+("" if data.getInvoiceID() is None else str(data.getInvoiceID())), style)],
-            [Paragraph("<b>DATA</b>", style), Paragraph("" if data.getInvoiceDate() is None else data.getInvoiceDate(), style)],
-            [Paragraph("<b>Scadenza Pagamento</b>", style), Paragraph("" if data.getPaymentDueDate() is None else data.getPaymentDueDate(), style)],
-        ]
-        t = Table(tableFlowables, colWidths=[width * ratio, width * (1 - ratio)])
-        t.setStyle(TableStyle())  
-        return t
+        return Paragraph("<b><font size=\"%d\">%s</font></b>" % (LARGE_FONT_SIZE, business.getName()), style)
     
-    def getToEndpointFlowable(self):
+    def getDocumentDetailsFlowable(self, data, width):
         pass
     
-    def getFromEndpointFlowable(self):
-        pass
+    def __getEndPointFlowable(self, data, label):
+        style = getSampleStyleSheet()["Normal"]
+        return Paragraph("<b><font size=\"%(size)d\">%(label)s</font></b><br/>%(name)s<br/>%(address)s<br/>%(city)s (%(province)s)<br/>%(country)s" %
+                         dict(size=MEDIUM_FONT_SIZE, name=data.getName(), label=label,
+                              address=data.getAddress(), city=data.getCity(),
+                              province=data.getProvince(), country=data.getCountry()), style)
+    
+    def getToEndpointFlowable(self, data):
+        return self.__getEndPointFlowable(data.getClient(), "To")
+    
+    def getFromEndpointFlowable(self, data):
+        return self.__getEndPointFlowable(data.getBusiness(), "From")
     
     def getDocumentItemsFlowable(self, itemsData, width):
-        pass
+        data = [["Price", "Qty", "Description", "Tax", "Total"]]
+        for item in itemsData:
+            data.append([item.getPrice(), item.getQuantity(), item.getDescription(), item.getTax(), item.getTotal()])
+        itemsFlowable = Table(data, colWidths=[0.2*width, 0.1*width, 0.4*width, 0.1*width, 0.2*width])
+        itemsFlowable.setStyle(TableStyle([("ALIGN", (-1,0), (-1,-1), "RIGHT"),
+                                           ("BACKGROUND", (0,0), (-1,0), gray),
+                                           ("LINEBELOW", (0,1), (-1,-1), BORDER_SIZE, BORDER_COLOR)]))
+        return itemsFlowable
     
-    def getDocumentTotals(self):
-        pass
+    def getDocumentTotals(self, data, width):
+        totals = Table([["Total before tax:", "%s €" % data.getTotalBeforeTax()],
+                        ["Total tax:", "%s €" % data.getTotalTax()],
+                        ["", ""],
+                        ["Total:", "%s €" % data.getTotal()]], colWidths=[0.5*width, 0.5*width])
+        totals.setStyle(TableStyle([("ALIGN", (-1,0), (-1,-1), "RIGHT"),
+                                    ("FONTSIZE", (0,0), (-1,-2), 16),
+                                    ("FONTSIZE", (0,-1), (-1,-1), 18)]))
+        return totals
+        
     
-    def getNotesFlowable(self):
-        pass
+    def getNotesFlowable(self, data):
+        style = getSampleStyleSheet()["Normal"]
+        return Paragraph("<b>Invoice notes:</b><br/>%s" %  data.getNote() if data.getNote() else "<i>No notes</i>", style)
     
--- novabill-report/src/template/tidy/invoice.py
+++ novabill-report/src/template/tidy/invoice.py
@@ -7,21 +7,33 @@ from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus.para import Paragraph
 from reportlab.platypus.tables import Table, TableStyle
 from template.tidy import TidyDocumentBuilder, BORDER_SIZE, \
-    BORDER_COLOR
+    BORDER_COLOR, LARGE_FONT_SIZE, SMALL_FONT_SIZE
 
 
 class TidyInvoiceBuilder(TidyDocumentBuilder):
-    pass
-#    def getDocumentDetailsFlowable(self, data, width, ratio):
-#        style = getSampleStyleSheet()["Normal"]
-#        tableFlowables = [
-#            [Paragraph("<b>FATTURA</b>", style), Paragraph("" if data.getInvoiceID() is None else str(data.getInvoiceID()), style)],
-#            [Paragraph("<b>DATA</b>", style), Paragraph("" if data.getInvoiceDate() is None else data.getInvoiceDate(), style)],
-#            [Paragraph("<b>Pagamento</b>", style), Paragraph(data.getHumanReadablePaymentType(), style)],
-#            [Paragraph("<b>Note Pagamento</b>", style), Paragraph("" if data.getPaymentNote() is None else data.getPaymentNote(), style)],
-#            [Paragraph("<b>Scadenza Pagamento</b>", style), Paragraph("" if data.getPaymentDueDate() is None else data.getPaymentDueDate(), style)],
-#            [Paragraph("<b>Note</b>", style), Paragraph("" if data.getNote() is None else data.getNote(), style)]
-#        ]
-#        t = Table(tableFlowables, colWidths=[width * ratio, width * (1 - ratio)])
-#        t.setStyle(TableStyle([("BOX", (0, 0), (-1, -1), BORDER_SIZE, BORDER_COLOR)]))
-#        return t
+    
+    def getDocumentDetailsFlowable(self, data, width):
+        style = getSampleStyleSheet()["Normal"]
+        t = Table([["", Paragraph("<b><font size=\"%d\">Invoice %d</font></b>" % (LARGE_FONT_SIZE, data.getInvoiceID()), style)],
+                   ["Date of Invoice:", data.getInvoiceDate()],
+                   ["P.O Number:", ""],
+                   ["Payment date:", data.getPaymentDueDate() if data.getPaymentDueDate() else ""]],
+                  colWidths=[width*0.5, width*0.5])
+        t.setStyle(TableStyle([("ALIGN", (0, 0), (0, -1), "RIGHT"),
+                               ("ALIGN", (1, 0), (1, -1), "LEFT")]))
+        return t
+    
+        #=======================================================================
+        # style = getSampleStyleSheet()["Normal"]
+        # tableFlowables = [
+        #   [Paragraph("<b>FATTURA</b>", style), Paragraph("" if data.getInvoiceID() is None else str(data.getInvoiceID()), style)],
+        #   [Paragraph("<b>DATA</b>", style), Paragraph("" if data.getInvoiceDate() is None else data.getInvoiceDate(), style)],
+        #   [Paragraph("<b>Pagamento</b>", style), Paragraph(data.getHumanReadablePaymentType(), style)],
+        #   [Paragraph("<b>Note Pagamento</b>", style), Paragraph("" if data.getPaymentNote() is None else data.getPaymentNote(), style)],
+        #   [Paragraph("<b>Scadenza Pagamento</b>", style), Paragraph("" if data.getPaymentDueDate() is None else data.getPaymentDueDate(), style)],
+        #   [Paragraph("<b>Note</b>", style), Paragraph("" if data.getNote() is None else data.getNote(), style)]
+        # ]
+        # t = Table(tableFlowables, colWidths=[width * ratio, width * (1 - ratio)])
+        # t.setStyle(TableStyle([("BOX", (0, 0), (-1, -1), BORDER_SIZE, BORDER_COLOR)]))
+        # return t
+        #=======================================================================
