5eb3898d1f335d8e6399e77e0fe2c1f0f80c4cfe
Risto Gligorov <risto.gligorov@novadart.com>
2012-11-30 21:22:23 +0100

    making XsrfTokenService operations thread-safe

--- novabill-server/src/main/java/com/novadart/novabill/service/XsrfTokenService.java
+++ novabill-server/src/main/java/com/novadart/novabill/service/XsrfTokenService.java
@@ -1,12 +1,9 @@
 package com.novadart.novabill.service;
 
 import java.security.NoSuchAlgorithmException;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-
 import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,24 +17,21 @@ public class XsrfTokenService {
 	private Set<String> getTokensSet(HttpSession session, String tokensSessionField){
 		Set<String> set = (Set<String>)session.getAttribute(tokensSessionField);
 		if(set == null){
-			set = Collections.synchronizedSet(new HashSet<String>());
+			set = new HashSet<String>();
 			session.setAttribute(tokensSessionField, set);
 		}
 		return set;
 	}
 	
-	public boolean verifyAndRemoveToken(String token, HttpSession session, String tokensSessionField){
+	public synchronized boolean verifyAndRemoveToken(String token, HttpSession session, String tokensSessionField){
 		Set<String> tokens = getTokensSet(session, tokensSessionField);
-		if(!tokens.contains(token))
-			return false;
-		tokens.remove(token);
-		return true;
+		return tokens.remove(token);
 	}
 	
-	public String generateToken(HttpSession session, String tokensSessionField) throws NoSuchAlgorithmException{
-		String token = tokenGenerator.generateToken();
+	public synchronized String generateToken(HttpSession session, String tokensSessionField) throws NoSuchAlgorithmException{
 		Set<String> tokens = getTokensSet(session, tokensSessionField);
-		tokens.add(token);
+		String token;
+		while(!tokens.add(token = tokenGenerator.generateToken()));
 		return token;
 	}
 
