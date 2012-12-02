b086ee34924c88d8c4dd93d5ef6d5e88261fc97f
Risto Gligorov <risto.gligorov@novadart.com>
2012-12-02 14:13:14 +0100

    improving synchronization of xsrf token service

--- novabill-server/src/main/java/com/novadart/novabill/service/XsrfTokenService.java
+++ novabill-server/src/main/java/com/novadart/novabill/service/XsrfTokenService.java
@@ -1,11 +1,13 @@
 package com.novadart.novabill.service;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.WebUtils;
 
 @Service
 public class XsrfTokenService {
@@ -15,23 +17,30 @@ public class XsrfTokenService {
 	
 	@SuppressWarnings("unchecked")
 	private Set<String> getTokensSet(HttpSession session, String tokensSessionField){
-		Set<String> set = (Set<String>)session.getAttribute(tokensSessionField);
-		if(set == null){
-			set = new HashSet<String>();
-			session.setAttribute(tokensSessionField, set);
+		Set<String> set;
+		synchronized (WebUtils.getSessionMutex(session)) {
+			set = (Set<String>) session.getAttribute(tokensSessionField);
+			if (set == null) {
+				set = Collections.synchronizedSet(new HashSet<String>());
+				session.setAttribute(tokensSessionField, set);
+			}
 		}
 		return set;
 	}
 	
-	public synchronized boolean verifyAndRemoveToken(String token, HttpSession session, String tokensSessionField){
+	public boolean verifyAndRemoveToken(String token, HttpSession session, String tokensSessionField){
 		Set<String> tokens = getTokensSet(session, tokensSessionField);
-		return tokens.remove(token);
+		boolean wasPresent = tokens.remove(token);
+		if(wasPresent)
+			session.setAttribute(tokensSessionField, tokens);
+		return wasPresent;
 	}
 	
-	public synchronized String generateToken(HttpSession session, String tokensSessionField) throws NoSuchAlgorithmException{
+	public String generateToken(HttpSession session, String tokensSessionField) throws NoSuchAlgorithmException{
 		Set<String> tokens = getTokensSet(session, tokensSessionField);
 		String token;
 		while(!tokens.add(token = tokenGenerator.generateToken()));
+		session.setAttribute(tokensSessionField, tokens);
 		return token;
 	}
 
