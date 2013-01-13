a3f5168c096d45406618c3e915e7ce6a2f5e073d
Giordano Battilana <giordano.battilana@novadart.com>
2013-01-13 18:37:20 +0100

    fixes #413

--- novabill-server/src/main/java/com/novadart/novabill/service/AccountStatusManagerService.java
+++ novabill-server/src/main/java/com/novadart/novabill/service/AccountStatusManagerService.java
@@ -6,16 +6,17 @@ import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.novadart.novabill.annotation.MailMixin;
 import com.novadart.novabill.domain.Business;
 import com.novadart.novabill.domain.security.RoleType;
 
 
-@Service
+//@Service
 @MailMixin
 public class AccountStatusManagerService {
 	
