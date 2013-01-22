40c6cce8e123b55b2b353f2df5fa576499dfdc46
Risto Gligorov <risto.gligorov@novadart.com>
2013-01-22 17:43:47 +0100

    logo thumbnail

--- novabill-server/src/main/java/com/novadart/novabill/web/mvc/BusinessLogoController.java
+++ novabill-server/src/main/java/com/novadart/novabill/web/mvc/BusinessLogoController.java
@@ -4,18 +4,18 @@ import java.awt.Dimension;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.im4java.core.IM4JavaException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.support.ServletContextResource;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.novadart.novabill.domain.Logo;
 import com.novadart.novabill.domain.Logo.LogoFormat;
 import com.novadart.novabill.service.UtilsService;
@@ -42,6 +41,18 @@ public class BusinessLogoController {
 	public static final int LOGO_SIZE_LIMIT = 1024 * 1024; // 1MB
 	
 	public static final LogoFormat DEFAULT_FORMAT = LogoFormat.JPEG;
+	
+	@Value("${logoThumbnail.format}")
+	private String logoThumbnailFormat;
+	
+	@Value("${logoThumbnail.width}")
+	private int logoThumbnailWidth;
+	
+	@Value("${logoThumbnail.height}")
+	private int logoThumbnailHeight;
+	
+	@Value("${logoThumbnail.folder}")
+	private String logoThumbnailFolder;
 	
 	@Autowired
 	private UtilsService utilsService;
@@ -67,6 +78,39 @@ public class BusinessLogoController {
 		response.setContentType("image/" + (logo == null? FilenameUtils.getExtension(noLogoImage.getPath()): logo.getFormat().name().toLowerCase()));
 		response.setHeader ("Content-Disposition", String.format("attachment; filename=\"%s\"", logo == null? FilenameUtils.getName(noLogoImage.getPath()): logo.getName()));
 		IOUtils.copy(is, response.getOutputStream());
+	}
+	
+	private void serveLogoThumbnail(InputStream logoThumbnailIS, HttpServletResponse response, String name) throws IOException{
+		response.setContentType("image/" + logoThumbnailFormat);
+		response.setHeader ("Content-Disposition", String.format("attachment; filename=\"%s\"", name));
+		IOUtils.copy(logoThumbnailIS, response.getOutputStream());
+	}
+	
+	private File getThumbnailFile(Long businessID){
+		return new File(logoThumbnailFolder, businessID.toString() + "." + logoThumbnailFormat);
+	}
+	
+	@RequestMapping(value = "/thumbnail", method = RequestMethod.GET)
+	@ResponseBody
+	public void getLogoThumbnail(HttpServletResponse response) throws IOException, InterruptedException, IM4JavaException, UnsupportedImageFormatException{
+		Long businessID = utilsService.getAuthenticatedPrincipalDetails().getBusiness().getId();
+		Logo logo = Logo.getLogoByBusinessID(businessID);
+		if(logo == null)
+			serveLogoThumbnail(noLogoImage.getInputStream(), response, FilenameUtils.getName(noLogoImage.getPath()));
+		else{
+			File logoThumbnailFile = getThumbnailFile(businessID);
+			if(!logoThumbnailFile.exists()){
+				File inFile = null;
+				try{
+					inFile = createTempFile("." + logo.getFormat().name(), true);
+					IOUtils.copy(new ByteArrayInputStream(logo.getData()), new FileOutputStream(inFile));
+					ImageUtils.resizeConvertImage(inFile, logoThumbnailWidth, logoThumbnailHeight, logoThumbnailFile);
+				}finally{
+					if(inFile != null) inFile.delete();
+				}
+			}
+			serveLogoThumbnail(new FileInputStream(logoThumbnailFile), response, FilenameUtils.getName(logoThumbnailFile.getPath()));
+		}
 	}
 	
 	private File createTempFile(String extension, boolean deleteOnExit) throws IOException{
@@ -114,6 +158,7 @@ public class BusinessLogoController {
 			logo.setData(IOUtils.toByteArray(new FileInputStream(outFile)));
 			logo.setBusinessID(businessID);
 			logo.persist();
+			ImageUtils.resizeConvertImage(inFile, logoThumbnailWidth, logoThumbnailHeight, getThumbnailFile(businessID));
 		} catch (IOException e) {
 			return String.valueOf(LogoUploadStatus.INTERNAL_ERROR.ordinal());
 		} catch (InterruptedException e) {
@@ -131,6 +176,9 @@ public class BusinessLogoController {
 		Logo oldLogo = Logo.getLogoByBusinessID(businessID);
 		if(oldLogo != null)
 			oldLogo.remove();
+		File thumbnailFile = getThumbnailFile(businessID);
+		if(thumbnailFile.exists())
+			thumbnailFile.delete();
 	}
 	
 	@RequestMapping(method = RequestMethod.DELETE)
