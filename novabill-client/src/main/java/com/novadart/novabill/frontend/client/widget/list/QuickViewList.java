1c8e754f47c726eb33367ace4604e6e6d574a910
Giordano Battilana <giordano.battilana@novadart.com>
2013-07-01 01:34:17 +0200

    fixes #524

--- novabill-client/src/main/java/com/novadart/novabill/frontend/client/widget/list/QuickViewList.java
+++ novabill-client/src/main/java/com/novadart/novabill/frontend/client/widget/list/QuickViewList.java
@@ -1,7 +1,9 @@
 package com.novadart.novabill.frontend.client.widget.list;
 
 import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.view.client.NoSelectionModel;
+import com.novadart.novabill.frontend.client.resources.ImageResources;
 import com.novadart.novabill.frontend.client.widget.list.resources.QuickViewListBundle;
 import com.novadart.novabill.frontend.client.widget.list.resources.QuickViewListResources;
 
@@ -14,7 +16,7 @@ public class QuickViewList<T> extends CellList<T> {
 	
 	public QuickViewList(QuickViewCell<T> cell) {
 		super(cell, QuickViewListResources.INSTANCE);
-		
+		setLoadingIndicator(new Image(ImageResources.INSTANCE.loader()));
 		setSelectionModel(new NoSelectionModel<T>());
 	}
 	
