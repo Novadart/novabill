44ef9ba4d2d9be45029d18d0daac64354cdb17d0
Giordano Battilana <giordano.battilana@novadart.com>
2013-10-25 18:35:32 +0200

    work on the Instrumentation Tests

--- novabill-android-it/src/main/java/com/novadart/novabill/android/test/NovabillDBHelperTest.java
+++ novabill-android-it/src/main/java/com/novadart/novabill/android/test/NovabillDBHelperTest.java
@@ -1,14 +1,15 @@
 package com.novadart.novabill.android.test;
 
-import com.novadart.novabill.android.content.provider.NovabillDBHelper;
+import org.springframework.util.Assert;
 
 import android.test.AndroidTestCase;
 
 public class NovabillDBHelperTest extends AndroidTestCase {
 	
-	private NovabillDBHelper dbHelper;
+//	private NovabillDBHelper dbHelper;
 	
 	 public void testOnCreate(){
+		 Assert.notNull(Boolean.TRUE);
 	 }
 	
 }
