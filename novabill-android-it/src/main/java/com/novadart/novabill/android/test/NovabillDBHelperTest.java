2879873a1c12c45f71dfb0e20e87242d2f4f18f4
Risto Gligorov <risto.gligorov@novadart.com>
2013-10-26 21:40:10 +0200

    novabill content provider tests

--- novabill-android-it/src/main/java/com/novadart/novabill/android/test/NovabillDBHelperTest.java
+++ novabill-android-it/src/main/java/com/novadart/novabill/android/test/NovabillDBHelperTest.java
@@ -2,14 +2,58 @@ package com.novadart.novabill.android.test;
 
 import org.springframework.util.Assert;
 
-import android.test.AndroidTestCase;
+import com.novadart.novabill.android.content.provider.DBSchema;
+import com.novadart.novabill.android.content.provider.NovabillContentProvider;
+import com.novadart.novabill.android.content.provider.NovabillContract;
+import com.novadart.novabill.android.content.provider.NovabillDBHelper;
 
-public class NovabillDBHelperTest extends AndroidTestCase {
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.test.AndroidTestCase;
+import android.test.ProviderTestCase2;
+import android.test.RenamingDelegatingContext;
+
+public class NovabillDBHelperTest extends ProviderTestCase2<NovabillContentProvider> {
+
+	private NovabillDBHelper dbHelper;
 	
-//	private NovabillDBHelper dbHelper;
+
+	public NovabillDBHelperTest() {
+		super(NovabillContentProvider.class, NovabillContract.AUTHORITY);
+	}
 	
-	 public void testOnCreate(){
-		 Assert.notNull(Boolean.TRUE);
-	 }
+	public NovabillDBHelperTest(Class<NovabillContentProvider> providerClass, String providerAuthority) {
+		super(providerClass, providerAuthority);
+	}
+
+
+
+
+	private boolean checkIfTableExists(SQLiteDatabase db, String tblName){
+		Cursor cursor = db.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name =?", new String[]{tblName});
+		boolean result = cursor != null && cursor.getCount() > 0;
+		cursor.close();
+		return result;
+	}
 	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
+		dbHelper = NovabillDBHelper.getInstance(context);
+		
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+
+	public void testOnCreate() {
+		SQLiteDatabase db = dbHelper.getReadableDatabase();
+		assertTrue(checkIfTableExists(db, DBSchema.UserTbl.TABLE_NAME));
+		assertTrue(checkIfTableExists(db, DBSchema.ClientTbl.TABLE_NAME));
+	}
+
 }
