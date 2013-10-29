384b09efc03d4f276897d83212f7aea1bd4df916
Risto Gligorov <risto.gligorov@novadart.com>
2013-10-29 18:49:12 +0100

    checking out novabill-server from develop branch

--- novabill-server/src/main/java/com/novadart/novabill/web/mvc/PrivateController.java
+++ novabill-server/src/main/java/com/novadart/novabill/web/mvc/PrivateController.java
@@ -1,77 +1,52 @@
 package com.novadart.novabill.web.mvc;
 
+import java.io.StringWriter;
+import java.util.concurrent.TimeUnit;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import com.novadart.novabill.domain.Business;
+import com.novadart.novabill.domain.dto.factory.BusinessDTOFactory;
+import com.novadart.novabill.domain.security.Principal;
+import com.novadart.novabill.service.UtilsService;
+import com.novadart.novabill.shared.client.dto.BusinessDTO;
 
 @Controller
 public class PrivateController {
-	
-	public static enum PAGES {
-		DASHBOARD, CLIENTS, ITEMS, PAYMENTS, SETTINGS, INVOICES, ESTIMATIONS, TRANSPORT_DOCUMENTS, CREDIT_NOTES
-	}
 
+	private static final ObjectMapper jsonSerializer = new ObjectMapper();
 
-	@RequestMapping(value = Urls.PRIVATE_HOME, method = RequestMethod.GET)
-	public ModelAndView dashboard(){
-		ModelAndView mav = new ModelAndView("private.dashboard");
-		mav.addObject("activePage", PAGES.DASHBOARD);
-		return mav;
-	}
-	
-	@RequestMapping(value = Urls.PRIVATE_CLIENTS, method = RequestMethod.GET)
-	public ModelAndView clients(){
-		ModelAndView mav = new ModelAndView("private.clients");
-		mav.addObject("activePage", PAGES.CLIENTS);
-		return mav;
-	}
-	
-	@RequestMapping(value = Urls.PRIVATE_DOCS_INVOICES, method = RequestMethod.GET)
-	public ModelAndView invoices(){
-		ModelAndView mav = new ModelAndView("private.invoices");
-		mav.addObject("activePage", PAGES.INVOICES);
-		return mav;
-	}
-	
-	@RequestMapping(value = Urls.PRIVATE_DOCS_ESTIMATIONS, method = RequestMethod.GET)
-	public ModelAndView estimations(){
-		ModelAndView mav = new ModelAndView("private.estimations");
-		mav.addObject("activePage", PAGES.ESTIMATIONS);
-		return mav;
-	}
-	
-	@RequestMapping(value = Urls.PRIVATE_DOCS_TRANSPORT_DOCUMENTS, method = RequestMethod.GET)
-	public ModelAndView transportDocuments(){
-		ModelAndView mav = new ModelAndView("private.transportDocuments");
-		mav.addObject("activePage", PAGES.TRANSPORT_DOCUMENTS);
-		return mav;
-	}
-	
-	@RequestMapping(value = Urls.PRIVATE_DOCS_CREDIT_NOTES, method = RequestMethod.GET)
-	public ModelAndView creditNotes(){
-		ModelAndView mav = new ModelAndView("private.creditNotes");
-		mav.addObject("activePage", PAGES.CREDIT_NOTES);
-		return mav;
-	}
-	
-	@RequestMapping(value = Urls.PRIVATE_ITEMS, method = RequestMethod.GET)
-	public ModelAndView items(){
-		ModelAndView mav = new ModelAndView("private.items");
-		mav.addObject("activePage", PAGES.ITEMS);
-		return mav;
-	}
-	
-	@RequestMapping(value = Urls.PRIVATE_PAYMENTS, method = RequestMethod.GET)
-	public ModelAndView payments(){
-		ModelAndView mav = new ModelAndView("private.payments");
-		mav.addObject("activePage", PAGES.PAYMENTS);
-		return mav;
-	}
-	
-	@RequestMapping(value = Urls.PRIVATE_FIRST_RUN, method = RequestMethod.GET)
-	public ModelAndView firstRun(){
-		ModelAndView mav = new ModelAndView("private.firstrun");
+	@Autowired
+	private UtilsService utilsService;
+
+	@Value("${devMode.enabled}")
+	private boolean devMode;
+
+	@RequestMapping(value = "/private", method = RequestMethod.GET)
+	public ModelAndView privateArea(){
+		ModelAndView mav = new ModelAndView("private");
+		Business business = Principal.findPrincipal(utilsService.getAuthenticatedPrincipalDetails().getId()).getBusiness();
+		String serializedBizz = null;
+		if(business != null) {
+			BusinessDTO businessDTO = BusinessDTOFactory.toDTO(business);
+			if (business != null) {
+				StringWriter sw = new StringWriter();
+				try {
+					jsonSerializer.writeValue(sw, businessDTO);
+					serializedBizz = sw.toString();
+				} catch (Exception e) {
+					return null;
+				}
+			}
+		}
+		mav.addObject("business", serializedBizz);
+		mav.addObject("daysToExpiration", business == null? null: business.getNonFreeExpirationDelta(TimeUnit.DAYS));
+		mav.addObject("notesBitMask", Principal.findPrincipal(utilsService.getAuthenticatedPrincipalDetails().getId()).getNotesBitMask());
+		mav.addObject("devMode", devMode);
 		return mav;
 	}
 
