1ef7c8888b5341ec89d3684025bfb3f7a6693583
Risto Gligorov <risto.gligorov@novadart.com>
2014-01-18 16:22:15 +0100

    Feature #700

--- novabill-server/src/main/java/com/novadart/novabill/report/ReportUtils.java
+++ novabill-server/src/main/java/com/novadart/novabill/report/ReportUtils.java
@@ -3,7 +3,11 @@ package com.novadart.novabill.report;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+
+import com.novadart.novabill.domain.TransportDocument;
 
 public class ReportUtils {
 	
@@ -11,6 +15,13 @@ public class ReportUtils {
 		DecimalFormat format = (DecimalFormat)NumberFormat.getNumberInstance(locale);
 		format.applyPattern("0.#");
 		return format.format(decimal.doubleValue());
+	}
+	
+	public static List<TransportDetails> createTransportDetailsDataSource(TransportDocument transDoc){
+		TransportDetails tranDetails = new TransportDetails(transDoc);
+		List<TransportDetails> result = new ArrayList<>(1);
+		result.add(tranDetails);
+		return result;
 	}
 
 }
