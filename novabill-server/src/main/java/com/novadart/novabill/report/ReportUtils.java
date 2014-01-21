7587d8b3fd353527486f9caf845982b48bf90eb6
Risto Gligorov <risto.gligorov@novadart.com>
2014-01-21 17:01:16 +0100

    Feature #697

--- novabill-server/src/main/java/com/novadart/novabill/report/ReportUtils.java
+++ novabill-server/src/main/java/com/novadart/novabill/report/ReportUtils.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
 import com.novadart.novabill.domain.TransportDocument;
 
 public class ReportUtils {
@@ -22,6 +24,23 @@ public class ReportUtils {
 		List<TransportDetails> result = new ArrayList<>(1);
 		result.add(tranDetails);
 		return result;
+	}
+	
+	public static String join(Object ... args){
+		if(args == null || args.length == 0)
+			throw new IllegalArgumentException();
+		String separator = args[0].toString();
+		List<String> tokens = new ArrayList<>(args.length - 1);
+		for(int i = 1; i < args.length; ++i)
+			tokens.add(Strings.emptyToNull(args[i].toString()));
+		Joiner joiner = Joiner.on(separator).skipNulls();
+		return joiner.join(tokens);
+	}
+	
+	public static String transformIfNotBlank(String pattern, Object arg){
+		if(Strings.isNullOrEmpty(pattern.trim()))
+			throw new IllegalArgumentException();
+		return arg == null || Strings.isNullOrEmpty(arg.toString())? "": String.format(pattern, arg);
 	}
 
 }
