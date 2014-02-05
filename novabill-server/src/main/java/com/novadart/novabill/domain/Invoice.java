d366e570ce6edd8cab5c368c5e876523ef21f9b7
Giordano Battilana <giordano.battilana@novadart.com>
2014-02-05 21:37:25 +0100

    fixes #787

--- novabill-server/src/main/java/com/novadart/novabill/domain/Invoice.java
+++ novabill-server/src/main/java/com/novadart/novabill/domain/Invoice.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.novadart.novabill.shared.client.dto.PaymentDateType;
+import com.novadart.novabill.shared.client.dto.PaymentDeltaType;
 
 
 /*
@@ -38,6 +39,10 @@ public class Invoice extends AbstractInvoice implements Serializable {
 	private PaymentDateType paymentDateGenerator;
 
 	private Integer paymentDateDelta;
+	
+    private PaymentDeltaType paymentDeltaType;
+	
+	private Integer secondaryPaymentDateDelta;
 
     @ManyToOne
     protected Business business;
@@ -70,6 +75,22 @@ public class Invoice extends AbstractInvoice implements Serializable {
 
 	public void setPaymentDateDelta(Integer paymentDateDelta) {
 		this.paymentDateDelta = paymentDateDelta;
+	}
+	
+	public PaymentDeltaType getPaymentDeltaType() {
+		return paymentDeltaType;
+	}
+
+	public void setPaymentDeltaType(PaymentDeltaType paymentDeltaType) {
+		this.paymentDeltaType = paymentDeltaType;
+	}
+
+	public Integer getSecondaryPaymentDateDelta() {
+		return secondaryPaymentDateDelta;
+	}
+
+	public void setSecondaryPaymentDateDelta(Integer secondaryPaymentDateDelta) {
+		this.secondaryPaymentDateDelta = secondaryPaymentDateDelta;
 	}
 
 	public String getPaymentTypeName() {
@@ -132,7 +153,7 @@ public class Invoice extends AbstractInvoice implements Serializable {
     public Invoice merge() {
         return merge(this);
     }
-    
+
     /*
      * End of active record functionality section
      * */
