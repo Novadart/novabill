6f8b951b30fd60b172c158f5e0468dbe4232d290
Risto Gligorov <risto.gligorov@novadart.com>
2014-05-07 16:45:05 +0200

    fixes #886

---
+++ novabill-server/src/main/java/com/novadart/novabill/web/mvc/ajax/PriceListController.java
@@ -0,0 +1,79 @@
+package com.novadart.novabill.web.mvc.ajax;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.novadart.novabill.annotation.RestExceptionProcessingMixin;
+import com.novadart.novabill.service.web.PriceListService;
+import com.novadart.novabill.shared.client.dto.PriceDTO;
+import com.novadart.novabill.shared.client.dto.PriceListDTO;
+import com.novadart.novabill.shared.client.exception.DataAccessException;
+import com.novadart.novabill.shared.client.exception.DataIntegrityException;
+import com.novadart.novabill.shared.client.exception.FreeUserAccessForbiddenException;
+import com.novadart.novabill.shared.client.exception.NoSuchObjectException;
+import com.novadart.novabill.shared.client.exception.NotAuthenticatedException;
+import com.novadart.novabill.shared.client.exception.ValidationException;
+import com.novadart.novabill.shared.client.tuple.Pair;
+
+@Controller
+@RestExceptionProcessingMixin
+@RequestMapping("/private/ajax/businesses/{businessID}/pricelists")
+public class PriceListController {
+
+	@Autowired
+	private PriceListService priceListService;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.OK)
+	public PriceListDTO get(@PathVariable Long id) throws NotAuthenticatedException, NoSuchObjectException, DataAccessException{
+		return priceListService.get(id);
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.OK)
+	public Long add(@RequestBody PriceListDTO priceListDTO) throws NotAuthenticatedException, ValidationException, FreeUserAccessForbiddenException, DataAccessException{
+		return priceListService.add(priceListDTO);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.OK)
+	public void update(@RequestBody PriceListDTO priceListDTO) throws NotAuthenticatedException, ValidationException, FreeUserAccessForbiddenException, DataAccessException, NoSuchObjectException{
+		priceListService.update(priceListDTO);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.OK)
+	public void remove(@PathVariable Long businessID, @PathVariable Long id) throws NotAuthenticatedException, DataAccessException, DataIntegrityException{
+		priceListService.remove(businessID, id);
+	}
+	
+	@RequestMapping(value = "/{id}/prices", method = RequestMethod.GET)
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.OK)
+	public Map<String, Pair<String, PriceDTO>> getPrices(Long businessID, Long id) throws NotAuthenticatedException, DataAccessException {
+		return priceListService.getPrices(businessID, id);
+	}
+	
+	@RequestMapping(value = "/{id}/clone", method = RequestMethod.POST)
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.OK)
+	public Long clonePriceList(@PathVariable Long businessID, @PathVariable Long id, @RequestParam String priceListName) throws NotAuthenticatedException, NoSuchObjectException, DataAccessException, ValidationException{
+		return priceListService.clonePriceList(businessID, id, priceListName);
+	}
+	
+}
