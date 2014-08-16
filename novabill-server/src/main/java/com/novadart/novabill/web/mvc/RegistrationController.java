6c73bdec940d8a9227e7ee2dc6a80f09f7d92ee3
Giordano Battilana <giordano.battilana@novadart.com>
2014-08-16 18:07:27 +0200

    fixes #989

--- novabill-server/src/main/java/com/novadart/novabill/web/mvc/RegistrationController.java
+++ novabill-server/src/main/java/com/novadart/novabill/web/mvc/RegistrationController.java
@@ -3,7 +3,9 @@ package com.novadart.novabill.web.mvc;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
+import com.novadart.novabill.annotation.MailMixin;
 import com.novadart.novabill.domain.Registration;
 import com.novadart.novabill.domain.security.Principal;
 import com.novadart.novabill.domain.security.RoleType;
@@ -32,29 +35,39 @@ import com.novadart.novabill.service.validator.RegistrationValidator;
  */
 @Controller
 @SessionAttributes("registration")
+@MailMixin
 public class RegistrationController{
-	
+
 	@Autowired
 	private RegistrationValidator validator;
 	
+	private static final String EMAIL_TEMPLATE_LOCATION = "mail-templates/registration-complete-notification.vm";
+
 	@InitBinder
 	public void setAllowedFields(WebDataBinder dataBinder) {
 		dataBinder.setDisallowedFields("id");
 	}
-	
+
 	@RequestMapping(value = Urls.PUBLIC_REGISTER, method = RequestMethod.GET)
 	public String login(Model model){
 		model.addAttribute("pageName", "Registrazione");
 		model.addAttribute("registration", new Registration());
 		return "frontend.register";
 	}
-	
+
+
+	private void sendRegistrationCompletedMail(Registration registration, Locale locale) throws UnsupportedEncodingException{
+		Map<String, Object> templateVars = new HashMap<String, Object>();
+		templateVars.put("registrationEmail", registration.getEmail());
+		sendMessage(registration.getEmail(), "Benvenuto in Novabill", templateVars, EMAIL_TEMPLATE_LOCATION);
+	}
+
 	@RequestMapping(value = Urls.PUBLIC_REGISTER, method = RequestMethod.POST)
 	public String processSubmit(@ModelAttribute("registration") Registration registration, BindingResult result,
 			SessionStatus status, Locale locale, Model model, HttpServletRequest request)
-			throws NoSuchAlgorithmException, UnsupportedEncodingException{
+					throws NoSuchAlgorithmException, UnsupportedEncodingException{
 		validator.validate(registration, result);
-		
+
 		if(result.hasErrors()) {
 			model.addAttribute("pageName", "Registrazione");
 			model.addAttribute("registration", registration);
@@ -66,9 +79,10 @@ public class RegistrationController{
 			principal.getGrantedRoles().add(RoleType.ROLE_BUSINESS_FREE);
 			principal.persist();
 			status.setComplete();
+			sendRegistrationCompletedMail(registration, locale);
 			return "forward:"+Urls.PUBLIC_LOGIN_CHECK + String.format("?j_username=%s&j_password=%s", URLEncoder.encode(registration.getEmail(), "UTF-8"), rawRassword);
 		}
 	}
-	
-	
+
+
 }
