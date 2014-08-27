f19cc6d90e39ca7a1302ce7874598f6ddaf4cccd
Risto Gligorov <risto.gligorov@novadart.com>
2014-08-27 14:21:52 +0200

    fixes #896

--- novabill-server/src/main/java/com/novadart/novabill/web/mvc/PublicPDFController.java
+++ novabill-server/src/main/java/com/novadart/novabill/web/mvc/PublicPDFController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JRException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,8 +20,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.novadart.novabill.domain.DocumentAccessToken;
 import com.novadart.novabill.domain.Invoice;
+import com.novadart.novabill.domain.Notification;
 import com.novadart.novabill.report.JasperReportKeyResolutionException;
 import com.novadart.novabill.service.web.InvoiceService;
+import com.novadart.novabill.shared.client.dto.NotificationType;
 import com.novadart.novabill.shared.client.exception.DataAccessException;
 import com.novadart.novabill.shared.client.exception.NoSuchObjectException;
 
@@ -31,7 +34,19 @@ public class PublicPDFController extends PDFController {
 	
 	@Autowired
 	private InvoiceService invoiceService;
+	
+	@Autowired
+	private ReloadableResourceBundleMessageSource messageSource;
 
+	private void createInvoiceDownloadedNotification(Invoice invoice, String message){
+		Notification notification = new Notification();
+		notification.setType(NotificationType.INVOICE_DOWNLOADED);
+		notification.setMessage(message);
+		notification.setBusiness(invoice.getBusiness());
+		invoice.getBusiness().getNotifications().add(notification);
+		notification.persist();
+	}
+	
 	@RequestMapping(method = RequestMethod.GET, value = "/invoices/{id}", produces = "application/pdf")
 	@ResponseBody
 	@Override
@@ -40,8 +55,12 @@ public class PublicPDFController extends PDFController {
 			HttpServletResponse response, Locale locale) throws IOException, DataAccessException, NoSuchObjectException, JasperReportKeyResolutionException, JRException {
 		if(DocumentAccessToken.findDocumentAccessTokens(id, token).size() == 0)
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		invoiceService.markViewedByClient(Invoice.findInvoice(id).getBusiness().getId(), id, System.currentTimeMillis());
-		return super.getInvoicePDF(id, token, print, response, locale);
+		ResponseEntity<byte[]> result = super.getInvoicePDF(id, token, print, response, locale);
+		Invoice invoice = Invoice.findInvoice(id);
+		invoiceService.markViewedByClient(invoice.getBusiness().getId(), id, System.currentTimeMillis());
+		createInvoiceDownloadedNotification(invoice,
+				String.format(messageSource.getMessage("invoices.downloaded.notification", null, locale), id, invoice.getClient().getName()));
+		return result;
 	}
 	
 	
