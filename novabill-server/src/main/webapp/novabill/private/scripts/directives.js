5f8829504f142678db827dae15904bb91eebafcf
Giordano Battilana <giordano.battilana@novadart.com>
2014-09-17 19:02:47 +0200

    added scroll to stats general page

--- novabill-server/src/main/webapp/novabill/private/scripts/directives.js
+++ novabill-server/src/main/webapp/novabill/private/scripts/directives.js
@@ -688,6 +688,19 @@ angular.module('novabill.directives',
 })
 
 
+.directive("nSlimScroll", [function () {
+
+    return {
+        restrict: "A",
+        link: function (scope, element, attrs) {
+            // instance-specific options
+            var opts = angular.extend({}, scope.$eval(attrs.slimScroll));
+            angular.element(element).slimScroll(opts);
+        }
+    };
+}])
+
+
 .directive('nEmailKeywordChooser', ['nConstants', function(nConstants) {
 	
     return {
