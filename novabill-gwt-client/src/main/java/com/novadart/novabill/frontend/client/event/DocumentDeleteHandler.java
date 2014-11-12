b2833dc5bc28f2edb32174c5501bc909cd4f60cb
Giordano Battilana <giordano.battilana@novadart.com>
2014-11-12 14:32:22 +0100

    dropped some obsolete GWT sources

--- novabill-gwt-client/src/main/java/com/novadart/novabill/frontend/client/event/DocumentDeleteHandler.java
+++
@@ -1,9 +0,0 @@
-package com.novadart.novabill.frontend.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface DocumentDeleteHandler extends EventHandler {
-	
-	public void onDocumentDelete(DocumentDeleteEvent event);
-
-}
--- novabill-gwt-client/src/main/java/com/novadart/novabill/frontend/client/event/PaymentUpdateHandler.java
+++
@@ -1,9 +0,0 @@
-package com.novadart.novabill.frontend.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface PaymentUpdateHandler extends EventHandler {
-	
-	public void onPaymentUpdate(PaymentUpdateEvent event);
-
-}
