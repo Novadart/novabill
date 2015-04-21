9103ac3a97d15abb2f957fbb9d37fa737889b913
Risto Gligorov <risto.gligorov@novadart.com>
2015-04-21 14:37:40 +0200

    Fixes #14

--- novabill-server/src/main/java/com/novadart/novabill/domain/AccountingDocumentComparator.java
+++ novabill-server/src/main/java/com/novadart/novabill/domain/AccountingDocumentComparator.java
@@ -1,5 +1,7 @@
 package com.novadart.novabill.domain;
 
+import com.google.common.base.Strings;
+
 import java.util.Comparator;
 
 public class AccountingDocumentComparator implements Comparator<AccountingDocument> {
@@ -13,10 +15,16 @@ public class AccountingDocumentComparator implements Comparator<AccountingDocume
 		int cmpYear = abInv1.getAccountingDocumentYear().compareTo(abInv2.getAccountingDocumentYear());
 		if(cmpYear > 0) return -1;
 		if(cmpYear < 0) return 1;
-		
+
+		String doc1Suffix = Strings.nullToEmpty(abInv1.getDocumentIDSuffix());
+		String doc2Suffix = Strings.nullToEmpty(abInv2.getDocumentIDSuffix());
+		int cmpDocIdCls= doc1Suffix.compareTo(doc2Suffix);
+		if(cmpDocIdCls != 0) return cmpDocIdCls;
+
 		int cmpDocId = abInv1.getDocumentID().compareTo(abInv2.getDocumentID());
 		if(cmpDocId > 0) return -1;
 		if(cmpDocId < 0) return 1;
+
 		return 0;
 	}
 
