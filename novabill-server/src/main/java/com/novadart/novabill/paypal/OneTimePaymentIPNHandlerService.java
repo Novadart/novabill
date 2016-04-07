df823988f71d53889249f47890f6080f66c2e5d1
Risto Gligorov <risto.gligorov@novadart.com>
2016-04-07 13:18:07 +0200

    Paypal payment: adding logging

--- novabill-server/src/main/java/com/novadart/novabill/paypal/OneTimePaymentIPNHandlerService.java
+++ novabill-server/src/main/java/com/novadart/novabill/paypal/OneTimePaymentIPNHandlerService.java
@@ -2,20 +2,23 @@ package com.novadart.novabill.paypal;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OneTimePaymentIPNHandlerService extends PayPalIPNHandlerService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(OneTimePaymentIPNHandlerService.class);
 	
 	private static final String WEB_ACCEPT = "web_accept";
 	
 	private static final String MC_CURRENCY = "mc_currency";
 	
-	private void handleError(String email, String message){}
-	
 	private boolean checkCurrencyAndAmount(Map<String, String> parametersMap, PaymentPlanDescriptor paymentPlan){
 		if(!parametersMap.get(MC_CURRENCY).equalsIgnoreCase("EUR")){
-			handleError(parametersMap.get(CUSTOM), "Currency doesn't match");
+			LOGGER.warn(String.format("Currency type for transaction %s does not match. Expected EUR, actual: %s",
+					parametersMap.get(TRANSACTION_ID), parametersMap.get(MC_CURRENCY)));
 			return false;
 		}
 		return true;
@@ -23,12 +26,18 @@ public class OneTimePaymentIPNHandlerService extends PayPalIPNHandlerService {
 
 	@Override
 	protected boolean check(String transactionType, Map<String, String> parametersMap) {
-		PaymentPlanDescriptor paymentPlan = paymentPlans.getPayPalPaymentPlanDescriptor(parametersMap.get(ITEM_NAME));
-		if(!transactionType.equals(WEB_ACCEPT))
+		if(!transactionType.equals(WEB_ACCEPT)) {
+			LOGGER.warn(String.format("Transaction type for transaction %s does not match. Expected %s, actual: %s",
+					parametersMap.get(TRANSACTION_ID), WEB_ACCEPT, transactionType));
 			return false;
-		if(!parametersMap.get(PAYMENT_STATUS).equals(COMPLETED)) //not completed, ignore
+		}
+		if(!parametersMap.get(PAYMENT_STATUS).equals(COMPLETED)) { //not completed, ignore
+			LOGGER.warn(String.format("Payment status for transaction %s not completed.",
+					parametersMap.get(TRANSACTION_ID)));
 			return false;
-		if(parametersMap.get(ITEM_NAME).equals(paymentPlan.getItemName()) && !checkCurrencyAndAmount(parametersMap, paymentPlan))
+		}
+		if(!checkCurrencyAndAmount(parametersMap,
+				paymentPlans.getPayPalPaymentPlanDescriptor(parametersMap.get(ITEM_NAME))))
 			return false;
 		return true;
 	}
